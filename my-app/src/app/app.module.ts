0fe94d50fe0f42f4176fe0cb11d549fc49f15294
darshan sidar <sidardarshan@gmail.com>
2018-06-20 11:17:11 +0530

    add the product and users blogs

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,15 +3,30 @@ import { NgModule } from '@angular/core';
 
 import {HttpModule} from '@angular/http'
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
+
+const appRoutes: Routes = [
+   { path: 'Product', component: ProductComponent },
+   { path: 'Users', component: UsersComponent },
+   { path: 'Orders', component: OrdersComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    UsersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     HttpModule, 
     HttpClientModule
   ],
